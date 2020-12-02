97d5ae9cd31d7c99b71a117713349b2c064d9f65
shaun <scoen@nla.gov.au>
2020-12-02 16:25:20 +1100

    Added nla-deploy.sh for standard jvmctl deployment

---
+++ nla-deploy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+export ORIGDIR=`pwd`
+
+echo "Build env $RAILS_ENV, unzip into $WEBROOT.. PWD $ORIGDIR"
+export PATH=$GEM_PATH/bin:$PATH
+
+cd $ORIGDIR
+
+RUBY_VERSION=`cat .ruby-version`
+echo "Checking rbenv Ruby version $RUBY_VERSION is installed."
+if [[ ! -d "/apps/etc/.rbenv/versions/$RUBY_VERSION" ]]; then
+  echo "Ruby $RUBY_VERSION is not installed. Installing with rbenv..."
+  rbenv install $RUBY_VERSION
+  echo "Finished installing Ruby $RUBY_VERSION; continuing deployment..."
+else
+  echo "Ruby $RUBY_VERSION found; continuing deployment..."
+fi
+
+gem install bundler -v 2.1.4
+bundle _2.1.4_ install --path $GEM_HOME
+bundle _2.1.4_ exec rake db:migrate RAILS_ENV=$RAILS_ENV
+RAILS_ENV=$RAILS_ENV bundle _2.1.4_ exec rake assets:precompile
+
+cp -R .bundle .ruby-version .ruby-gemset * $1
