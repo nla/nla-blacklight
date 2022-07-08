58fe65ce4b24c116c94cd8891bcf6504eabd98f2
Yetrina Battad <hello@yetti.io>
2022-07-08 20:15:08 +1000

    feat: prevents user registrations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,7 @@ class User < ApplicationRecord
   include Blacklight::User
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :recoverable, :rememberable, :validatable
 
   # Method added by Blacklight; Blacklight uses #to_s on your
   # user class to get a user-displayable login/identifier for
