20f3c632ec98debc1d14c9df0597b298ff9ce864
Yetrina Battad <hello@yetti.io>
2022-07-15 08:06:04 +1000

    fix: changed user columns to not nullable

--- db/migrate/20220713044706_add_patron_fields_to_user.rb
+++ db/migrate/20220713044706_add_patron_fields_to_user.rb
@@ -13,8 +13,8 @@ class AddPatronFieldsToUser < ActiveRecord::Migration[7.0]
 
     remove_column :users, :email
     remove_column :users, :encrypted_password
-    add_column :users, :patron_id, :bigint, :after => :id
-    add_column :users, :voyager_id, :bigint, :after => :patron_id
+    add_column :users, :patron_id, :bigint, :after => :id, null: false
+    add_column :users, :voyager_id, :bigint, :after => :patron_id, null: false
 
     add_index :users, :patron_id, unique: true
     add_index :users, :voyager_id, unique: true
