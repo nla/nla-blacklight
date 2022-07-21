d82f1a5652128e6919673065d1508aa958e65aa5
Yetrina Battad <hello@yetti.io>
2022-07-21 10:37:14 +1000

    refactor: fixes incorrect directory in cucumber VCR config

--- features/support/vcr.rb
+++ features/support/vcr.rb
@@ -1,7 +1,7 @@
 require "vcr"
 
 VCR.configure do |c|
-  c.cassette_library_dir = "spec/cassettes"
+  c.cassette_library_dir = "features/cassettes"
   c.hook_into :webmock
   c.ignore_localhost = true
 end
