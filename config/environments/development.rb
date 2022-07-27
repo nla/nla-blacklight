2f879bc0f703a48064feb754a073944a8217b5d8
Yetrina Battad <hello@yetti.io>
2022-07-28 08:38:04 +1000

    build: configure staging environment deployment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -78,6 +78,12 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
+  if ENV["RAILS_LOG_TO_STDOUT"].present?
+    logger = ActiveSupport::Logger.new($stdout)
+    logger.formatter = config.log_formatter
+    config.logger = ActiveSupport::TaggedLogging.new(logger)
+  end
+
   # allow requests to
   config.hosts << "delong.nla.gov.au"
   config.hosts << "catalogue-devel.nla.gov.au"
