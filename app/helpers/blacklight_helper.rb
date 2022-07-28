0979ddb8e3ec111d880fb31092ca4b16f0903b9c
Yetrina Battad <hello@yetti.io>
2022-07-28 10:32:09 +1000

    style: adds warning banner for devel and test envs

--- app/helpers/blacklight_helper.rb
+++ app/helpers/blacklight_helper.rb
@@ -7,12 +7,12 @@ module BlacklightHelper
 
   def render_page_title
     prefix = case ENV["RAILS_ENV"]
-    when "development" then "[DEVEL] "
+    when "development" then "[DEV] "
     when "staging" then "[TEST] "
     else ""
     end
     # rubocop:disable Rails/HelperInstanceVariable
-    prefix.to_s + (content_for(:page_title) if content_for?(:page_title)) || @page_title || application_name
+    prefix + ((content_for(:page_title) if content_for?(:page_title)) || @page_title || application_name)
     # rubocop:enable Rails/HelperInstanceVariable
   end
 end
