69b41e02a237e580855a4fb92edeb3da16a38370
Yetrina Battad <hello@yetti.io>
2022-08-09 15:20:26 +1000

    fix: fixes building of Google search link

--- app/models/search_link.rb
+++ app/models/search_link.rb
@@ -57,16 +57,22 @@ class SearchLink
 
   def build_google_query(url)
     unless url.empty?
-      query = [phrase_query(url), extract_domain(url)]
+      title_start = @document.get_marc_derived_field("245a").first
+      query = [phrase_query(title_start), extract_domain(url)]
 
       cited_authors = @document.get_marc_derived_field("100a:110a:111a:700a:710a:711a")
       cited_authors.each do |author|
         query << process_author(author)
       end
 
-      file_extension = Addressable::URI.parse(url).extname
-      unless file_extension.nil?
-        query << "pdf" if file_extension.downcase == "pdf"
+      # Uses Addressable::URI because the standard URI library cannot
+      # handle query strings in the URL when parsing for the file extension
+      uri = Addressable::URI.parse(url) || nil
+      unless uri.nil?
+        file_extension = uri.extname
+        unless file_extension.nil?
+          query << "pdf" if file_extension.downcase == "pdf"
+        end
       end
 
       GOOGLE_BASE_URL % [join_query(query)] unless query.empty?
@@ -82,7 +88,27 @@ class SearchLink
   end
 
   def extract_domain(url)
-    URI.parse(url).host unless url.empty?
+    result = ""
+    unless url.empty?
+      host = URI.parse(url).host || ""
+
+      if host.present?
+        segments = host.split(".").reverse
+
+        # Heuristic: if the domain ends in a 2-letter country code, take the last
+        # three segments (e.g. www.nla.gov.au -> nla.gov.au).  Otherwise, just take
+        # two (e.g. www.irs.princeton.edu -> princeton.edu).
+        #
+        # This might be a bit extreme for something like www.library.act.gov.au
+        # (truncating to act.gov.au), but probably best for search recall to
+        # over-truncate than to under-truncate.
+        slice_len = segments[0].length == 2 ? 3 : 2
+
+        result = segments[0, slice_len].reverse.join(".").downcase
+      end
+    end
+
+    result
   end
 
   def trim_garbage(value)
@@ -97,7 +123,7 @@ class SearchLink
   def process_author(author)
     result = []
     unless author.empty?
-      names = author.split(/ +/)
+      names = author.downcase.split(/ +/)
 
       unless names.size > 3
         names.each do |name|
