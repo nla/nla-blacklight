d551c572ae7a3172dc89953cd109425960562589
Yetrina Battad <hello@yetti.io>
2022-08-25 11:48:21 +1000

    fix: fix declaration of instance variables

--- lib/blacklight/solr/cloud/repository.rb
+++ lib/blacklight/solr/cloud/repository.rb
@@ -11,6 +11,7 @@ module Blacklight
         ZNODE_COLLECTIONS = "/collections".freeze
 
         def initialize(blacklight_config)
+          Rails.logger.info "initializing Blacklight::Solr::Cloud::Repository"
           super(blacklight_config)
           setup_zk
         end
@@ -22,7 +23,8 @@ module Blacklight
         private
 
         def setup_zk
-          @zk = ZK.new(ENV["ZK_HOST"] || "localhost:2181", {chroot: :do_nothing})
+          Rails.logger.info "initializing Zookeeper"
+          @zk ||= ZK.new(ENV["ZK_HOST"] || "localhost:2181", {chroot: :do_nothing})
           collection = ENV["SOLR_COLLECTION"] || "blacklight"
 
           init_collection_state_watcher collection
@@ -61,20 +63,15 @@ module Blacklight
 
         def update_urls(collection)
           synchronize do
-            @all_urls, @leader_urls = available_urls collection
+            @all_urls = []
+            @leader_urls = []
+            all_nodes.each do |node|
+              next unless active_node?(node)
+              url = "#{node["base_url"]}/#{collection}"
+              @leader_urls << url if leader_node? node
+              @all_urls << url
+            end
           end
-        end
-
-        def available_urls(collection)
-          leader_urls = []
-          all_urls = []
-          all_nodes.each do |node|
-            next unless active_node?(node)
-            url = "#{node["base_url"]}/#{collection}"
-            leader_urls << url if leader_node? node
-            all_urls << url
-          end
-          [all_urls, leader_urls]
         end
 
         def all_nodes
@@ -100,7 +97,7 @@ module Blacklight
 
         def update_live_nodes
           synchronize do
-            @live_nodes = {}
+            @live_nodes ||= {}
             @zk.children(ZNODE_LIVE_NODES, watch: true).each do |node|
               @live_nodes[node] = true
             end
