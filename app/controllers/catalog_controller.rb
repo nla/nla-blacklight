7bf0efc476185cae2533dc03e6ee4b64b987bb43
Yetrina Battad <hello@yetti.io>
2022-11-02 09:04:12 +1100

    test: fixes ISBN/ISSN tests; makes 880 field use conditional

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -168,7 +168,7 @@ class CatalogController < ApplicationController
     config.add_show_field "scale", label: "Scale", accessor: :scale
     config.add_show_field "printer", label: "Printer", accessor: :printer
     config.add_show_field label: "Description", field: "description", accessor: :description
-    config.add_show_field "isbn", label: "ISBN", accessor: :isbn, helper_method: :list
+    config.add_show_field "isbn", label: "ISBN", accessor: :isbn, helper_method: :unstyled_list
     config.add_show_field "invalid_isbn", label: "Invalid ISBN", accessor: :invalid_isbn
     config.add_show_field "issn", label: "ISSN", accessor: :issn
     config.add_show_field "invalid_issn", label: "Invalid ISSN", accessor: :invalid_issn, helper_method: :unstyled_list
