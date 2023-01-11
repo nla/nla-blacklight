e25b80e0c94ebc264270f762cfd98f218892a9b7
Yetrina Battad <hello@yetti.io>
2023-01-11 16:34:12 +1100

    test: refactor cucumber tests as rspec tests

---
+++ spec/features/search_bar_spec.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.feature "Search via search bar" do
+  scenario "from outside advanced search page" do
+    visit "/"
+    fill_in "q", with: "cats"
+    click_button "Search"
+    expect(page).to have_content("Search Results")
+  end
+
+  scenario "from advanced search page" do
+    visit blacklight_advanced_search_engine.advanced_search_path
+    expect(page).to have_content("Advanced Search")
+    fill_in "q", with: "cats"
+    click_button "Search"
+    expect(page).to have_content("Search Results")
+  end
+end
