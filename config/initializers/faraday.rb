0d3681e91d9a786462b23fa04123aaf55825ccee
Yetrina Battad <hello@yetti.io>
2023-01-13 08:34:09 +1100

    refactor: set default User-Agent for Faraday requests

--- config/initializers/faraday.rb
+++ config/initializers/faraday.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require "faraday"
+
 # Make sure Faraday requests include a non-default User-Agent, since it will break the specs
 # every time Faraday is upgraded.
 Faraday.default_connection_options = {headers: {user_agent: "nla-blacklight/#{Rails.configuration.version}"}}
