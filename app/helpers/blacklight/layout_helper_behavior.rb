13ed1ade487e38d44017101a45a29686f7190358
Yetrina Battad <hello@yetti.io>
2023-02-22 15:26:27 +1100

    fix: fixes Rubocop issues

--- app/helpers/blacklight/layout_helper_behavior.rb
+++ app/helpers/blacklight/layout_helper_behavior.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 # Methods added to this helper will be available to all templates in the hosting
 # application
 module Blacklight
@@ -15,7 +16,7 @@ module Blacklight
     # Attributes to add to the <html> tag (e.g. lang and dir)
     # @return [Hash]
     def html_tag_attributes
-      { lang: I18n.locale }
+      {lang: I18n.locale}
     end
 
     ##
@@ -29,14 +30,14 @@ module Blacklight
     # Classes used for sizing the main content of a Blacklight page
     # @return [String]
     def main_content_classes
-      'col-md-8 col-lg-9'
+      "col-md-8 col-lg-9"
     end
 
     ##
     # Classes used for sizing the sidebar content of a Blacklight page
     # @return [String]
     def sidebar_classes
-      'page-sidebar col-md-4 col-lg-3'
+      "page-sidebar col-md-4 col-lg-3"
     end
 
     ##
@@ -44,7 +45,7 @@ module Blacklight
     # overwritten to return 'container-fluid' for Bootstrap full-width layout
     # @return [String]
     def container_classes
-      'container'
+      "container"
     end
   end
 end
