68955c90cb7440d5cc424d18653ab135683f8f96
Yetrina Battad <hello@yetti.io>
2023-03-02 16:17:04 +1100

    test: fix test

--- spec/features/search_bar_spec.rb
+++ spec/features/search_bar_spec.rb
@@ -12,7 +12,7 @@ RSpec.feature "Search via search bar" do
 
   scenario "from advanced search page" do
     visit blacklight_advanced_search_engine.advanced_search_path
-    expect(page).to have_content("Advanced Search")
+    expect(page).to have_content("Advanced search")
     expect(page).not_to have_css(".search-query-form")
   end
 end
