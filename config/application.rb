9775d6a7fbedcdf0e909cb80f853d16e71c9baab
Yetrina Battad <12688+yetti@users.noreply.github.com>
2023-05-12 08:32:31 +1000

    chore(main): release 1.0.0

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ if %w[development test].include? ENV["RAILS_ENV"]
 end
 
 module NlaBlacklight
-  VERSION = "0.2.0"
+  VERSION = "1.0.0"
 
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
