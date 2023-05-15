4637ad54fb6bae504ff8c0af508dc893fcdd4823
Yetrina Battad <hello@yetti.io>
2023-05-15 11:26:35 +1000

    feat: display request details for serials

--- app/components/request_details_component.rb
+++ app/components/request_details_component.rb
@@ -8,4 +8,34 @@ class RequestDetailsComponent < ViewComponent::Base
     @iteration = holding_iteration
   end
 
+  def recent_item_issue_held
+    most_recent = @holding["holdingsStatements"].last
+
+    if most_recent.present?
+      if most_recent["statement"].present?
+        most_recent["statement"]
+      elsif most_recent["note"].present?
+        most_recent["enumerationAndChronology"]
+      end
+    end
+  end
+
+  def items_issues_held
+    @holding["holdingsStatements"].map do |statement|
+      if statement["statement"].present?
+        statement["statement"]
+      elsif statement["note"].present?
+        statement["note"]
+      end
+    end
+  end
+
+  def supplements
+    @holding["holdingsStatementsForSupplements"].pluck("note")
+  end
+
+  def indexes
+    @holding["holdingsStatementsForIndexes"].pluck("note")
+  end
+
 end
