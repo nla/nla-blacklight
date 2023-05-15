1aa8c997305a0c92bd77a95cd4278555d37ea830
Yetrina Battad <hello@yetti.io>
2023-05-16 09:11:16 +1000

    refactor: use different field for request pickup location

--- spec/features/search_bar_spec.rb
+++ spec/features/search_bar_spec.rb
@@ -3,13 +3,6 @@
 require "rails_helper"
 
 RSpec.feature "Search via search bar" do
-  # scenario "from outside advanced search page" do
-  #   visit "/catalog"
-  #   fill_in "q", with: "cats"
-  #   click_button "Search"
-  #   expect(page).to have_content("Search Results")
-  # end
-
   scenario "from advanced search page" do
     visit blacklight_advanced_search_engine.advanced_search_path
     expect(page).to have_content("Advanced search")
