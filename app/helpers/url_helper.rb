8bfbbe885a3b12b68f938d88e87b52c23cbfd73b
Yetrina Battad <hello@yetti.io>
2023-05-19 11:18:18 +1000

    feat: truncate titles to 175 characters on index search page

---
+++ app/helpers/url_helper.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+module UrlHelper
+  include Blacklight::UrlHelperBehavior
+
+  def link_to_document(doc, field_or_opts = nil, opts = {counter: nil})
+    label = case field_or_opts
+    when NilClass
+      document_presenter(doc).heading
+    when Hash
+      opts = field_or_opts
+      document_presenter(doc).heading
+    when Proc, Symbol
+      Deprecation.warn(self, "passing a #{field_or_opts.class} to link_to_document is deprecated and will be removed in Blacklight 8")
+      Deprecation.silence(Blacklight::IndexPresenter) do
+        index_presenter(doc).label field_or_opts, opts
+      end
+    else # String
+      field_or_opts
+    end
+
+    if current_page?(search_catalog_path)
+      label = label.truncate(175, separator: " ")
+    end
+
+    Deprecation.silence(Blacklight::UrlHelperBehavior) do
+      link_to label, url_for_document(doc), document_link_params(doc, opts)
+    end
+  end
+end
