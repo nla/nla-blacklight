13343804d6127a799b07fe6d30e60c91a49d0e34
Yetrina Battad <hello@yetti.io>
2023-05-22 15:45:03 +1000

    fix: refactor EDS bento search engine

--- app/search_engines/bento_search/ebsco_eds_engine.rb
+++ app/search_engines/bento_search/ebsco_eds_engine.rb
@@ -39,7 +39,6 @@ class BentoSearch::EbscoEdsEngine
       results.total_items = 0
       return results
     end
-    required_hit_count = args[:per_page].present? ? [args[:per_page], 1].max : 1
     per_page = args[:per_page]
 
     query = if configuration[:query_prefix].present?
@@ -55,79 +54,48 @@ class BentoSearch::EbscoEdsEngine
 
     response = session.search(sq)
     total_hits = response.stat_total_hits
-    total_tested_hits = [response.stat_total_hits.to_i, 100].min
 
     results.total_items = total_hits.to_i
 
-    catch :enough_hits do
-      found = 0
-      max_page = (total_tested_hits / per_page).ceil
-      (0..max_page).each { |p|
-        sq = {
-          query: q,
-          page: p,
-          results_per_page: per_page
-        }
+    response.records.each do |rec|
+      item = BentoSearch::ResultItem.new
+      item.link_is_fulltext = true
 
-        response = session.search(sq, add_actions: true)
+      item.title = (rec.eds_title.presence || I18n.t("bento_search.eds.record_not_available"))
+      item.title = prepare_ebsco_eds_payload(item.title, true)
 
-        response.records.each do |rec|
-          # duplicates creep in unless
-          found_already = false
-          results.each do |check|
-            if check.unique_id == rec.id
-              found_already = true
-              break
-            end
-          end
-          next if found_already
+      item.abstract = rec.eds_abstract
+      item.abstract = prepare_ebsco_eds_payload(item.abstract, true)
 
-          found += 1
-          if required_hit_count.present?
-            throw :enough_hits if found > required_hit_count
-          elsif found > 1
-            throw :enough_hits
-          end
+      item.unique_id = rec.id
+      authors = rec.eds_authors
+      authors.each do |author|
+        item.authors << BentoSearch::Author.new(display: author)
+      end
+      item.link = rec.eds_plink
 
-          item = BentoSearch::ResultItem.new
-          item.link_is_fulltext = true
-
-          item.title = (rec.eds_title.presence || I18n.t("bento_search.eds.record_not_available"))
-          item.title = prepare_ebsco_eds_payload(item.title, true)
-
-          item.abstract = rec.eds_abstract
-          item.abstract = prepare_ebsco_eds_payload(item.abstract, true)
-
-          item.unique_id = rec.id
-          authors = rec.eds_authors
-          authors.each do |author|
-            item.authors << BentoSearch::Author.new(display: author)
-          end
-          item.link = rec.eds_plink
-
-          item.format_str = rec.eds_publication_type
-          item.doi = rec.eds_document_doi
-          if rec.eds_page_start.present?
-            item.start_page = rec.eds_page_start.to_s
-            if rec.eds_page_count.present?
-              item.end_page = (rec.eds_page_start.to_i + rec.eds_page_count.to_i - 1).to_s
-            end
-          end
-          date = rec.eds_publication_date
-          if date.present?
-            ymd = date.split("-").map(&:to_i)
-            item.publication_date = Date.new(ymd[0], ymd[1], ymd[2])
-          end
-          year = rec.eds_publication_year
-          if year.present?
-            item.year = year
-          end
-          item.source_title = rec.eds_source_title
-          item.volume = rec.eds_volume
-          results << item
+      item.format_str = rec.eds_publication_type
+      item.doi = rec.eds_document_doi
+      if rec.eds_page_start.present?
+        item.start_page = rec.eds_page_start.to_s
+        if rec.eds_page_count.present?
+          item.end_page = (rec.eds_page_start.to_i + rec.eds_page_count.to_i - 1).to_s
         end
-      }
-    end # enough hits already
+      end
+      date = rec.eds_publication_date
+      if date.present?
+        ymd = date.split("-").map(&:to_i)
+        item.publication_date = Date.new(ymd[0], ymd[1], ymd[2])
+      end
+      year = rec.eds_publication_year
+      if year.present?
+        item.year = year
+      end
+      item.source_title = rec.eds_source_title
+      item.volume = rec.eds_volume
+      results << item
+    end
+
     results
   end
 
