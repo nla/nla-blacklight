69ab7fae88eee50d39296ba46bd6589db22ba87f
Yetrina Battad <hello@yetti.io>
2023-05-25 14:17:55 +1000

    fix: fix config for EDS bento title search

--- config/initializers/bento_search.rb
+++ config/initializers/bento_search.rb
@@ -10,14 +10,13 @@ BentoSearch.register_engine("ebsco_eds_keyword") do |conf|
 end
 
 BentoSearch.register_engine("ebsco_eds_title") do |conf|
-  conf.engine = "BentoSearch::EdsPublicationEngine"
+  conf.engine = "BentoSearch::EdsEngine"
   conf.user_id = ENV["EDS_USER"]
   conf.password = ENV["EDS_PASS"]
   conf.auth = true
   conf.highlighting = false
   conf.profile = ENV["EDS_PROFILE"]
-  conf.publication_base_url = "https://eds-api.ebscohost.com/edsapi/publication/"
-  conf.title = "eResources (Publications)"
+  conf.title = "eResources (Title)"
   conf.base_url = "https://eds-api.ebscohost.com/edsapi/rest/"
 end
 
