010be8f339a62a80b11030e89237fbab47513542
Yetrina Battad <hello@yetti.io>
2023-06-13 12:21:59 +1000

    feat: integrate stimulus-clipboard button

--- app/components/citation_component.rb
+++ app/components/citation_component.rb
@@ -2,9 +2,9 @@
 
 class CitationComponent < ::Blacklight::Component
   DEFAULT_FORMATS = {
-    'blacklight.citation.mla': :export_as_mla_citation_txt,
-    'blacklight.citation.apa': :export_as_apa_citation_txt,
-    'blacklight.citation.chicago': :export_as_chicago_citation_txt
+    "blacklight.citation.mla": :export_as_mla_citation_txt,
+    "blacklight.citation.apa": :export_as_apa_citation_txt,
+    "blacklight.citation.chicago": :export_as_chicago_citation_txt
   }.freeze
 
   with_collection_parameter :document
@@ -12,9 +12,5 @@ class CitationComponent < ::Blacklight::Component
   def initialize(document:, formats: DEFAULT_FORMATS)
     @document = document
     @formats = formats.select { |_k, v| @document.respond_to?(v) }
-  end
-
-  def persistent_id_link
-    link_to "https://nla.gov.au/nla.cat-vn#{@document.id}", "https://nla.gov.au/nla.cat-vn#{@document.id}"
   end
 end
