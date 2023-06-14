602cf510ce5ba00620b9615265a768461327df85
Yetrina Battad <hello@yetti.io>
2023-06-14 16:21:24 +1000

    refactor: remove unnecessary function in clipboard controller

--- app/javascript/controllers/clipboard_controller.js
+++ app/javascript/controllers/clipboard_controller.js
@@ -1,12 +1,6 @@
 import Clipboard from 'stimulus-clipboard'
 
 export default class extends Clipboard {
-  connect() {
-    super.connect()
-    console.log('Do what you want here.')
-  }
-
-  // Function to override on copy.
   copy() {
     event.preventDefault()
 
