cd60b250e87357820acfe106f0e6ecddd1692a91
scott-yeadon <syeadon@nla.gov.au>
2023-06-20 10:25:53 +1000

    stop quotes crashing search

--- lib/blacklight_advanced_search/parsing_nesting_parser.rb
+++ lib/blacklight_advanced_search/parsing_nesting_parser.rb
@@ -16,9 +16,10 @@ module BlacklightAdvancedSearch::ParsingNestingParser
 
   # BLAC-326 add boosts to the anchored title index fields
   def add_anchored_title_boosts(param_hash, query)
-    param_hash[:bq] = "anchored_title_tsi:\"FINLLFIIJQ " + query + " AICULEDSSUL\"^600 OR anchored_title_only_tsi:\"FINLLFIIJQ " +
-      query + " AICULEDSSUL\"^500 OR anchored_title_no_format_tsi:\"FINLLFIIJQ " + query +
-      " AICULEDSSUL\"^500 OR left_anchored_title_tsi:\"FINLLFIIJQ " + query + "\"^800"
+    cleaned_query = query.gsub('"', "")
+    param_hash[:bq] = "anchored_title_tsi:\"FINLLFIIJQ " + cleaned_query + " AICULEDSSUL\"^600 OR anchored_title_only_tsi:\"FINLLFIIJQ " +
+      cleaned_query + " AICULEDSSUL\"^500 OR anchored_title_no_format_tsi:\"FINLLFIIJQ " + cleaned_query +
+      " AICULEDSSUL\"^500 OR left_anchored_title_tsi:\"FINLLFIIJQ " + cleaned_query + "\"^800"
   end
 
   def local_param_hash(key, config)
