7a65c4aa19ed76f66cd5c2b67812d6c1379c8fd1
Yetrina Battad <hello@yetti.io>
2023-07-11 13:29:25 +1000

    feat: adds Feedback widget to sidebar

---
+++ app/components/feedback_component.rb
@@ -0,0 +1,4 @@
+# frozen_string_literal: true
+
+class FeedbackComponent < ViewComponent::Base
+end
---
+++ spec/components/feedback_component_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe FeedbackComponent, type: :component do
+  it "renders the error reporting link" do
+    render_inline(described_class.new)
+
+    expect(rendered_component).to have_link(I18n.t("feedback.report_error"), href: ENV["FEEDBACK_ERROR_URL"])
+  end
+end
