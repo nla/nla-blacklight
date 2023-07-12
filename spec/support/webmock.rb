562a21cb78b765b0b19f47197b516eeb405fcd00
Yetrina Battad <hello@yetti.io>
2023-07-13 09:46:43 +1000

    test: fixing tests

--- spec/support/webmock.rb
+++ spec/support/webmock.rb
@@ -303,6 +303,6 @@ RSpec.configure do |config|
           "Accept-Encoding" => "gzip;q=1.0,deflate;q=0.6,identity;q=0.3"
         }
       )
-      .to_return(status: 200, body: "{\"requestlimitReached\": \"false\"}", headers: {})
+      .to_return(status: 200, body: "{\"requestLimitReached\": \"false\"}", headers: {})
   end
 end
