4d36b95aedad0d8e90ae7c873c0658e607ab6b17
Yetrina Battad <hello@yetti.io>
2023-08-02 11:24:07 +1000

    chore: reconfigure rack-mini-profiler

--- config/initializers/rack-mini-profiler.rb
+++ config/initializers/rack-mini-profiler.rb
@@ -1,3 +1,6 @@
 require "rack-mini-profiler"
 
-Rack::MiniProfiler.config.storage = Rack::MiniProfiler::MemoryStore
+if Rails.env.production?
+  Rack::MiniProfiler.config.storage_options = {url: ENV["REDIS_URL"]}
+  Rack::MiniProfiler.config.storage = Rack::MiniProfiler::RedisStore
+end
