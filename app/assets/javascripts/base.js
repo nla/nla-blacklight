ce16ac01f1b1dff6c5d5f8a5cd0cc4cdb3f12a7d
Yetrina Battad <hello@yetti.io>
2023-08-07 10:32:14 +1000

    feat: style blacklight range limit in modal

--- app/assets/javascripts/base.js
+++ app/assets/javascripts/base.js
@@ -5,10 +5,6 @@
 // Twitter Typeahead for autocomplete
 //= require twitter/typeahead
 //= require bootstrap
-//= require blacklight/blacklight
-// For blacklight_range_limit built-in JS, if you don't want it you don't need
-// this:
-//= require 'blacklight_range_limit'
 
 $(function() {
   $("#jumpbar .nav-link a").click(function(event) {
