f9ff012dc572f40a1dc057f4b6a1e5cb7f364e96
Yetrina Battad <hello@yetti.io>
2023-08-09 08:10:32 +1000

    fix: remove initializer for rack-mini-profiler

--- config/initializers/rack-mini-profiler.rb
+++
@@ -1,6 +0,0 @@
-require "rack-mini-profiler"
-
-if Rails.env.production?
-  Rack::MiniProfiler.config.storage_options = {url: ENV["REDIS_URL"]}
-  Rack::MiniProfiler.config.storage = Rack::MiniProfiler::RedisStore
-end
