9eac8ad86e927270eac9f3ccc997a62ff8890a4b
Yetrina Battad <hello@yetti.io>
2023-11-13 09:50:38 +1100

    fix: staff password is not editable

--- app/components/user_details_field_component.rb
+++ app/components/user_details_field_component.rb
@@ -1,12 +1,18 @@
 # frozen_string_literal: true
 
 class UserDetailsFieldComponent < ViewComponent::Base
+  PASSWORD_MASK = ""
+
   def initialize(attribute:, details:, value: nil, editable: true)
     super
     @attribute = attribute
     @details = details
     @label = I18n.t("account.settings.#{attribute}.label")
-    @value = value || @details.instance_values[attribute]
+    @value = if @attribute == "password"
+      PASSWORD_MASK # ensure we don't display anything for the password
+    else
+      value || @details.instance_values[attribute]
+    end
     @editable = editable
   end
 
