0213afd8db4279757f0f97eca7a9e7addb469ba1
Yetrina Battad <hello@yetti.io>
2023-12-05 09:56:11 +1100

    fix: style blacklight range limit modal graph

--- app/javascript/blacklight_custom.js
+++ app/javascript/blacklight_custom.js
@@ -38,9 +38,45 @@ $(function() {
       return;
     }
 
-    console.error('Server error:', this.url, jqXHR.status, errorThrown);
     var contents = '<div class="modal-header">' + '<div class="modal-title">There was a problem with your request.</div>' + '<button type="button" class="blacklight-modal-close btn-close close" data-dismiss="modal" aria-label="Close">' + '  <span aria-hidden="true">&times;</span>' + '</button></div>' + ' <div class="modal-body"><p>Expected a successful response from the server, but got an error</p>' + '<pre>' + this.type + ' ' + this.url + "\n" + jqXHR.status + ': ' + errorThrown + '</pre></div>';
     $(Blacklight.Modal.modalSelector).find('.modal-content').html(contents);
     Blacklight.Modal.show();
   };
+
+  BlacklightRangeLimit.turnIntoPlot = function turnIntoPlot(container, wait_for_visible) {
+    // flot can only render in a a div with a defined width.
+    // for instance, a hidden div can't generally be rendered in (although if you set
+    // an explicit width on it, it might work)
+    //
+    // We'll count on later code that catch bootstrap collapse open to render
+    // on show, for currently hidden divs.
+
+    // for some reason width sometimes return negative, not sure
+    // why but it's some kind of hidden.
+    if (container.width() > 0) {
+      var height = container.width() * BlacklightRangeLimit.display_ratio;
+
+      // Need an explicit height to make flot happy.
+      container.height( height )
+
+      $('.blrl-plot-config').each(function() {
+        console.log('setting plot config', $(this));
+        $(this).data('plot-config', {
+          selection: { color: '#46474A' },
+          colors: ['#ffffff'],
+          series: { lines: { fillColor: 'rgba(112,57,150, 0.8)' }},
+          grid: { color: '#677078', tickColor: '#f4f5f6', borderWidth: 1 }
+        });
+      });
+
+      BlacklightRangeLimit.areaChart($(container));
+
+      $(container).trigger(BlacklightRangeLimit.redrawnEvent);
+    }
+    else if (wait_for_visible > 0) {
+      setTimeout(function() {
+        BlacklightRangeLimit.turnIntoPlot(container, wait_for_visible - 50);
+      }, 50);
+    }
+  }
 });
