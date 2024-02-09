0f61ca38612dc08293e3cc3cbc2df4a8451c05ee
Yetrina Battad <hello@yetti.io>
2024-02-09 13:32:20 +1100

    fix: typo in Blacklight::Exceptions::RecordNotFound

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -451,7 +451,7 @@ class CatalogController < ApplicationController
               _, @document = search_service.fetch(params[:id])
               # if for some reason we can't find the document title, just use the title from the eResource entry
               t("offsite.other", title: @document.fetch("title_tsim", [@eresource[:entry]["title"]])&.first&.strip)
-            rescue Blacklight::Exception::RecordNotFound
+            rescue Blacklight::Exceptions::RecordNotFound
               t("offsite.other", title: @eresource[:entry]["title"]&.strip)
             end
           end
