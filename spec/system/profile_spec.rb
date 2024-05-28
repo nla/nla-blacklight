8cf8e4a4baccb9f0bd256bea98b345619c5edec3
Yetrina Battad <hello@yetti.io>
2024-05-28 17:55:15 +1000

    fix: setting user details during edit

---
+++ spec/system/profile_spec.rb
@@ -0,0 +1,48 @@
+require "system_helper"
+
+RSpec.describe "Profile" do
+  before do
+    sign_in create(:user, :patron, name_given: "Blacklight", name_family: "Test")
+  end
+
+  context "when patron has mobile and landline phone numbers" do
+    before do
+      user_details = IO.read("spec/files/profile/user_details.json")
+      WebMock.stub_request(:get, /catservices.test\/catalogue-services\/folio\/user/)
+        .to_return(status: 200, body: user_details, headers: {"Content-Type" => "application/json"})
+    end
+
+    it "displays both phone numbers" do
+      visit account_profile_url
+
+      expect(page).to have_content("0412345678")
+      expect(page).to have_content("0398765432")
+    end
+
+    describe "deleting a phone number" do
+      before do
+        WebMock.stub_request(:post, /catservices.test\/catalogue-services\/folio\/user\/updateDetails/)
+          .to_return(body: '{"status":"OK"}')
+      end
+
+      it "deletes a single number without error" do
+        visit account_profile_url
+
+        click_link "Change my phone number"
+
+        expect(page).to have_content("Update your phone number")
+        expect(page).to have_content("Current phone number")
+        expect(page).to have_content("0398765432")
+
+        fill_in "user_details_phone", with: ""
+
+        click_button "Update"
+
+        expect(page).not_to have_content(I18n.t("account.settings.update.errors.any_phone"))
+
+        # validate page has redirected back to profile page
+        expect(page).to have_content("Your Profile")
+      end
+    end
+  end
+end
