5e582761354a9b9170d8865235046b2298289b08
Yetrina Battad <hello@yetti.io>
2024-06-21 15:48:47 +1000

    feat: upgrade to Rails 7.1 and Ruby 3.3

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,13 +1,2 @@
 # Based on production
-require Rails.root.join("config", "environments", "production")
-
-Rails.application.configure do
-  # Use a real queuing backend for Active Job (and separate queues per environment).
-  # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "nla_blacklight_staging"
-
-  # allow requests to
-  config.hosts.clear
-  config.hosts << "localhost"
-  config.hosts << ".nla.gov.au"
-end
+require_relative "production"
