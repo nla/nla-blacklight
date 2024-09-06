1b45ef15a4e9749cc430ab925d1ff1ff003d0895
Jonathan Shaw <JonathanShaw@users.noreply.github.com>
2024-09-06 11:06:46 +1000

    chore(main): release nla-blacklight 3.3.0

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,7 @@ if %w[development test].include? ENV["RAILS_ENV"]
 end
 
 module NlaBlacklight
-  VERSION = "3.3.1"
+  VERSION = "3.3.0"
 
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
