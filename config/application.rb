6f105225ada46946a76a31580e2ab2b74273f3e9
CEdwardsBlasikiewicz <cedwards@nla.gov.au>
2024-12-09 09:01:58 +1100

    chore: prepare release

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,7 @@ if %w[development test].include? ENV["RAILS_ENV"]
 end
 
 module NlaBlacklight
-  VERSION = "3.5.0"
+  VERSION = "3.6.0"
 
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
