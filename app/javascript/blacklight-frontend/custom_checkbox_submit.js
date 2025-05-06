c7487ef2063adbfbaca47e4f9da0d6259e46f80a
calumedwardsblasikiewicz <cedwards@nla.gov.au>
2025-05-06 11:16:37 +1000

    `toggle` functionality working, label change broken.

--- app/javascript/blacklight-frontend/custom_checkbox_submit.js
+++ app/javascript/blacklight-frontend/custom_checkbox_submit.js
@@ -1,22 +1,8 @@
-/* Converts a "toggle" form, with single submit button to add/remove
-   something, like used for Bookmarks, into an AJAXy checkbox instead.
-   Apply to a form. Does require certain assumption about the form:
-    1) The same form 'action' href must be used for both ADD and REMOVE
-       actions, with the different being the hidden input name="_method"
-       being set to "put" or "delete" -- that's the Rails method to pretend
-       to be doing a certain HTTP verb. So same URL, PUT to add, DELETE
-       to remove. This plugin assumes that.
-       Plus, the form this is applied to should provide a data-doc-id
-       attribute (HTML5-style doc-*) that contains the id/primary key
-       of the object in question -- used by plugin for a unique value for
-       DOM id's.
-  Uses HTML for a checkbox compatible with Bootstrap 4.
-   new CheckboxSubmit(document.querySelector('form.something')).render()
-*/
-export default class CheckboxSubmit {
-  constructor(form) {
-    this.form = form
-  }
+// app/javascript/blacklight-frontend/custom_checkbox_submit.js
+import CheckboxSubmit from 'blacklight-frontend/checkbox_submit';
+
+export default class CCheckboxSubmit extends CheckboxSubmit {
+
 
   clicked(evt) {
     this.spanTarget.innerHTML = this.form.getAttribute('data-inprogress')
@@ -46,6 +32,7 @@ export default class CheckboxSubmit {
     })
   }
 
+
   get checked() {
     return (this.form.querySelectorAll('input[name=_method][value=delete]').length != 0)
   }
@@ -55,15 +42,15 @@ export default class CheckboxSubmit {
   }
 
   get labelTarget() {
-    return this.form.querySelector('[data-checkboxsubmit-target="label"]')
+    return this.form.querySelector('[data-ccheckboxsubmit-target="label"]')
   }
 
   get checkboxTarget() {
-    return this.form.querySelector('[data-checkboxsubmit-target="checkbox"]')
+    return this.form.querySelector('[data-ccheckboxsubmit-target="checkbox"]')
   }
 
   get spanTarget() {
-    return this.form.querySelector('[data-checkboxsubmit-target="span"]')
+    return this.form.querySelector('[data-ccheckboxsubmit-target="span"]')
   }
 
   bookmarksCounter() {
