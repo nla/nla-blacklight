49a3642953ea1f920dffb87a26c41116bffa3e1c
scott-yeadon <syeadon@nla.gov.au>
2025-07-11 09:04:03 +1000

    manually alter schema version

--- db/patrons_schema.rb
+++ db/patrons_schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_04_08_045725) do
+ActiveRecord::Schema[7.2].define(version: 2023_04_26_043927) do
   create_table "accounts", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
