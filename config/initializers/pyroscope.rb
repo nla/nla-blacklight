e0ce7c63eef3301f135afbce38cf4f7d4868b836
sdarvell <sdarvell@nla.gov.au>
2025-10-01 16:31:37 +1000

    Update pyroscope.rb

--- config/initializers/pyroscope.rb
+++ config/initializers/pyroscope.rb
@@ -1,15 +1,9 @@
-begin
-  # Ensure we're using the global gem path
-  gem 'pyroscope'
-  require 'pyroscope'
+require 'pyroscope'
 
-  app_name   = ENV.fetch('PYROSCOPE_APP_NAME', 'catalogue')
-  server_addr = ENV.fetch('PYROSCOPE_SERVER_ADDRESS', 'https://pyroscope.apps.dev-containers.nla.gov.au')
+app_name   = ENV.fetch('PYROSCOPE_APP_NAME', 'catalogue')
+server_addr = ENV.fetch('PYROSCOPE_SERVER_ADDRESS', 'https://pyroscope.apps.dev-containers.nla.gov.au')
 
-  Pyroscope.configure do |config|
-    config.application_name = app_name
-    config.server_address   = server_addr
-  end
-rescue Gem::LoadError, LoadError => e
-  Rails.logger.warn "Pyroscope gem not available globally: #{e.message}" if defined?(Rails)
+Pyroscope.configure do |config|
+  config.application_name = app_name
+  config.server_address   = server_addr
 end
