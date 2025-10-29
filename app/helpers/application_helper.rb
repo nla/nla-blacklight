4e8842ad34e1c834a62f065238ea5e6b99467909
jshaw <jshaw@nla.gov.au>
2025-10-29 12:05:06 +1100

    update link messaging for new onsite login reqs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -101,15 +101,11 @@ module ApplicationHelper
             <path d="M3.5 11.5a3.5 3.5 0 1 1 3.163-5H14L15.5 8 14 9.5l-1-1-1 1-1-1-1 1-1-1-1 1H6.663a3.5 3.5 0 0 1-3.163 2zM2.5 9a1 1 0 1 0 0-2 1 1 0 0 0 0 2z"/>
           </svg>
         ICON
-        if user_location == :offsite
           if current_user
             t("eresource.remote_access.logged_in")
           else
             t("eresource.remote_access.logged_out")
           end
-        else
-          t("eresource.onsite")
-        end
       else
         icon = <<~ICON
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-building-fill" viewBox="0 0 16 16">
@@ -119,7 +115,11 @@ module ApplicationHelper
         if user_location == :offsite
           t("eresource.offsite")
         else
-          t("eresource.onsite")
+          if current_user
+            t("eresource.remote_access.logged_in")
+          else
+            t("eresource.remote_access.logged_out")
+          end
         end
       end
     end
