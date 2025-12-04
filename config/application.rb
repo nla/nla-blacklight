94b6f769214fe779c39df25defaa57ee3191dbb9
CEdwardsBlasikiewicz <cedwards@nla.gov.au>
2025-12-04 13:22:09 +1100

    post-release commit

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,7 @@ if %w[development test].include? ENV["RAILS_ENV"]
 end
 
 module NlaBlacklight
-  VERSION = "4.3.0"
+  VERSION = "4.4.0"
 
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
