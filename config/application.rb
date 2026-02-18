9352ef49fd1b227ea026c50df1c7a78e6f5f9a08
CEdwardsBlasikiewicz <cedwards@nla.gov.au>
2026-02-19 09:43:11 +1100

    prep hotfix

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,7 @@ if %w[development test].include? ENV["RAILS_ENV"]
 end
 
 module NlaBlacklight
-  VERSION = "4.5.0"
+  VERSION = "4.4.1"
 
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
