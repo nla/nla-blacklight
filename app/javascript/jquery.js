06e4ccb30b6faeb4db0ec55cad3d626ad28616f0
CEdwardsBlasikiewicz <cedwards@nla.gov.au>
2026-02-19 09:50:21 +1100

    prep hotfix

--- app/javascript/jquery.js
+++ app/javascript/jquery.js
@@ -1,3 +1,9 @@
 import jquery from 'jquery'
 window.jQuery = jquery
 window.$ = jquery
+
+if (typeof jquery.isFunction !== 'function') {
+    jquery.isFunction = function(obj) {
+        return typeof obj === 'function'
+    }
+}
