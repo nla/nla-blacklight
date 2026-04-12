ccf198375dbc4b3078efed3ad6e3bf57798c7c5d
CEdwardsBlasikiewicz <cedwards@nla.gov.au>
2026-04-13 08:44:06 +1000

    dont show guest eresources to logged in patrons

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -31,6 +31,19 @@ module ApplicationHelper
     result
   end
 
+  # Returns the appropriate eResources URL based on the user's authentication status.
+  # Logged-in users are routed through the offsite handler so their session is applied
+  # and they don't see the "Welcome Guest" banner on EBSCO.
+  # Guests are sent to the standard guest URL.
+  def eresources_link_url
+    ebsco_eresources_url = ENV["EBSCO_ERESOURCES_URL"]
+    if current_user.present? && ebsco_eresources_url.present?
+      (id: "000000", url: ebsco_eresources_url)
+    else
+      ENV.fetch("ERESOURCES_GUEST_URL", "https://www.nla.gov.au/eresources")
+    end
+  end
+
   def show_search_bar?
     !current_page?(root_path) &&
       !current_page?(advanced_search_catalog_path) &&
