d7a8b5a3cfcd5b001d18df0577a179cf3163f7ec
CEdwardsBlasikiewicz <cedwards@nla.gov.au>
2026-08-13 09:18:02 +1000

    fixed for real this time

--- config/application.rb
+++ config/application.rb
@@ -29,7 +29,7 @@ if %w[development test].include? ENV["RAILS_ENV"]
 end
 
 module NlaBlacklight
-  VERSION = "4.5.3"
+  VERSION = "4.5.4"
 
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
