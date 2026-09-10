fd606ead5a356535516b5863152666ef2a02beae
CEdwardsBlasikiewicz <cedwards@nla.gov.au>
2026-09-10 10:49:35 +1000

    fix rufus cleanup cron

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -6,11 +6,11 @@ require "rufus/scheduler"
 
 scheduler = Rufus::Scheduler.new
 
-cron_schedule = ENV.fetch("SESSION_CLEANUP_CRON", "*/30 * * * *")
+cron_schedule = ENV.fetch("SESSION_CLEANUP_CRON", "*/30 * * * *").gsub(/\A["']|["']\z/, "")
 
 scheduler.cron(cron_schedule) do
   Rails.logger.info "Starting scheduled session cleanup"
-  Rake::Task["db:sessions:trim_in_batches"].invoke
+  Rails.application.rake_task["db:sessions:trim_in_batches"].invoke
   Rails.logger.info "Scheduled session cleanup completed"
 rescue => e
   Rails.logger.error "Session cleanup failed: #{e.message}"
